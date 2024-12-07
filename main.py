20c43e96e7102c9ffb26276640f1ec4eaa6d27ed
roupvsp <mamatmuktarova@gmail.com>
2024-12-07 19:17:30 +0300

    Абдумалик: ДОБАВИЛ ФАЙЛ module_malik.py; ИЗМЕНИЛ ФАЙЛ main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+from module_malik import STUDENTS_GRADES, STUDENTS_LIST
+
+def show_students(students):
+    for i in students:
+        print(i)
+
+show_students(STUDENTS_LIST)
---
+++ module_malik.py
@@ -0,0 +1,17 @@
+STUDENTS_LIST = [
+  "Esen M.",
+  "Altynai A.",
+  "Azamat T.",
+  "Kylych S.",
+  "Arsen K.",
+]
+
+MAX_SCORE = 30
+
+STUDENTS_GRADES = (
+  (15, MAX_SCORE),
+  (12, MAX_SCORE),
+  (30, MAX_SCORE),
+  (1, MAX_SCORE),
+  (99, MAX_SCORE),
+)
